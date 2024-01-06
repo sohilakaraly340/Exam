9909907173bee574b1b4fdb17a1b2f58e4574fdf
sohilakaraly340 <sohilakaraly958@gmail.com>
2024-01-05 18:52:09 -0800

    update timeOut page

--- script/timeOut.js
+++ script/timeOut.js
@@ -1,4 +1,14 @@
+const userEmail = localStorage.getItem('currentUserEmail') ? localStorage.getItem('currentUserEmail') : "";
+
 const users = localStorage.getItem('formData') ? JSON.parse(localStorage.getItem('formData')) : [];
-var firstName = users[users.length-1].firstName;
-var lastName = users[users.length-1].lastName;
+
+var firstName="" ,lastName="";
+
+users.map(user => {
+    if(user.email===userEmail){
+      firstName = user.firstName;
+      lastName = user.lastName;
+    }
+    
+});
 $("#userName").text(`sorry ${firstName} ${lastName} timeout!!`);
