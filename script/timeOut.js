61d82265f7fd1ffc5572f7806d96597e87d81fbc
sohilakaraly340 <sohilakaraly958@gmail.com>
2024-01-05 19:11:42 -0800

    add animation

--- script/timeOut.js
+++ script/timeOut.js
@@ -1,14 +1,16 @@
-const userEmail = localStorage.getItem('currentUserEmail') ? localStorage.getItem('currentUserEmail') : "";
+$(document).ready(function () {
+    const userEmail = localStorage.getItem('currentUserEmail') || "";
+    const users = localStorage.getItem('formData') ? JSON.parse(localStorage.getItem('formData')) : [];
 
-const users = localStorage.getItem('formData') ? JSON.parse(localStorage.getItem('formData')) : [];
+    let firstName = "", lastName = "";
 
-var firstName="" ,lastName="";
+    users.forEach(user => {
+        if (user.email === userEmail) {
+            firstName = user.firstName;
+            lastName = user.lastName;
+        }
+    });
 
-users.map(user => {
-    if(user.email===userEmail){
-      firstName = user.firstName;
-      lastName = user.lastName;
-    }
-    
+    $("#userName").text(`Sorry ${firstName} ${lastName}, timeout!!`);
+    $(".cont").slideDown(1200);
 });
-$("#userName").text(`sorry ${firstName} ${lastName} timeout!!`);
